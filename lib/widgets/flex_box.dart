64990063c167bd28fb56122ad287e88c1a28ff1f
Utsav-56 <utsavpokhrel200@gmail.com>
2025-05-17 16:07:32 +0545

    Including flex models in same file

--- lib/widgets/flex_box.dart
+++ lib/widgets/flex_box.dart
@@ -1,6 +1,56 @@
 import 'package:flutter/material.dart';
-import 'package:up_ui/models/flex_directions.dart';
 import 'package:up_ui/models/border_model.dart';
+
+enum FlexDirection {
+  row,
+  column,
+}
+
+enum AlignHorizontal {
+  start,
+  center,
+  end,
+  spaceBetween,
+  spaceAround,
+  spaceEvenly,
+}
+
+enum AlignVertical {
+  start,
+  center,
+  end,
+  stretch,
+}
+
+MainAxisAlignment mapMainAxisAlignment(AlignHorizontal align) {
+  switch (align) {
+    case AlignHorizontal.center:
+      return MainAxisAlignment.center;
+    case AlignHorizontal.end:
+      return MainAxisAlignment.end;
+    case AlignHorizontal.spaceBetween:
+      return MainAxisAlignment.spaceBetween;
+    case AlignHorizontal.spaceAround:
+      return MainAxisAlignment.spaceAround;
+    case AlignHorizontal.spaceEvenly:
+      return MainAxisAlignment.spaceEvenly;
+    default:
+      return MainAxisAlignment.start;
+  }
+}
+
+CrossAxisAlignment mapCrossAxisAlignment(AlignVertical align) {
+  switch (align) {
+    case AlignVertical.center:
+      return CrossAxisAlignment.center;
+    case AlignVertical.end:
+      return CrossAxisAlignment.end;
+    case AlignVertical.stretch:
+      return CrossAxisAlignment.stretch;
+    default:
+      return CrossAxisAlignment.start;
+  }
+}
 
 class FlexBox extends StatelessWidget {
   final List<Widget> children;
